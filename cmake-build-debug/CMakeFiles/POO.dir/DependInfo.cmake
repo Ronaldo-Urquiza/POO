65989a49350b1040f1091e168b32f7c573d2511d
Ronaldo Urquiza <80072240+Ronaldo-Urquiza@users.noreply.github.com>
2025-01-08 18:55:07 -0300

    Compatibilizando

---
+++ cmake-build-debug/CMakeFiles/POO.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Ronaldo Urquiza/Desktop/POO/POO em C++20/ATV3/q15/Agenda.cpp" "CMakeFiles/POO.dir/POO_em_C++20/ATV3/q15/Agenda.cpp.obj" "gcc" "CMakeFiles/POO.dir/POO_em_C++20/ATV3/q15/Agenda.cpp.obj.d"
+  "C:/Users/Ronaldo Urquiza/Desktop/POO/POO em C++20/ATV3/q15/main.cpp" "CMakeFiles/POO.dir/POO_em_C++20/ATV3/q15/main.cpp.obj" "gcc" "CMakeFiles/POO.dir/POO_em_C++20/ATV3/q15/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
